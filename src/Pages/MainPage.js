f6c6cb791d92621426887da5594ea33c3e70ddc9
kyuugoko <rafael.logrado@gmail.com>
2023-05-12 15:38:46 +0100

    fix: MainPage cleanup

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,7 +1,5 @@
 import React from "react";
-import NavBar from "../Layout/Navbar/NavBar";
 import styled from "styled-components";
-import Button from "../components/Button/Button";
 
 const Main = styled.main`
   background-color: ${(props) => props.theme.darkGrey};
@@ -11,13 +9,7 @@ const Main = styled.main`
 `;
 
 const MainPage = () => {
-  return (
-    <Main>
-      <NavBar />
-      <Button preset="primary">FIND OUT</Button>
-      <Button preset="secondary">SIGN UP</Button>
-    </Main>
-  );
+  return <Main></Main>;
 };
 
 export default MainPage;
