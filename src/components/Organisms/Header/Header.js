72c4c8a0e332ffe5b2838b66592a8065eb335330
kyuugoko <rafael.logrado@gmail.com>
2023-05-17 19:15:17 +0100

    fix: header color on scroll

--- src/components/Organisms/Header/Header.js
+++ src/components/Organisms/Header/Header.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { HeaderEl } from "./Header.styled.js";
 import NavBar from "../../Molecules/navbar/NavBar.js";
 import Logo from "../../Atoms/logo/Logo.js";
 
 const Header = () => {
+  const [bgColor, setBgColor] = useState(false);
+
+  const changeBgColor = () => {
+    window.scrollY >= window.innerHeight ? setBgColor(true) : setBgColor(false);
+  };
+
+  console.log(window.innerHeight);
+
+  window.addEventListener("scroll", changeBgColor);
+
   return (
-    <HeaderEl>
+    <HeaderEl className={bgColor ? "header_bg" : ""}>
       <Logo />
       <NavBar />
     </HeaderEl>
