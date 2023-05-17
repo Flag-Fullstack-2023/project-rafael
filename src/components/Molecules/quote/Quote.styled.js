5a84679bf9277572b93744738eaf0447073a172f
kyuugoko <rafael.logrado@gmail.com>
2023-05-17 01:18:18 +0100

    fix: queries and styling

--- src/components/Molecules/quote/Quote.styled.js
+++ src/components/Molecules/quote/Quote.styled.js
@@ -1,21 +1,48 @@
 import styled from "styled-components";
+import { breakpoints } from "../../../utils/breakpoints";
 
 export const QuoteEl = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(280px, 80rem));
   position: absolute;
   color: white;
-  width: 100rem;
-  top: 20%;
-  left: 5%;
+  padding: 2rem;
+  top: 0;
+  bottom: 0;
+  margin: auto 5%;
+  align-items: center;
+  max-height: 70vh;
 
   .quote__text {
-    font-size: 6.5rem;
+    font-size: 7rem;
     font-weight: 600;
   }
   .quote__author {
-    position: absolute;
-    bottom: -5%;
-    right: 55%;
+    align-self: start;
+    justify-self: end;
+  }
+
+  @media ${breakpoints.forBigTablet} {
+    .quote__author {
+      right: 10%;
+    }
+  }
+
+  @media (max-width: 526px) {
+    .quote__text {
+      font-size: 5rem;
+    }
+  }
+
+  @media ${breakpoints.forBigMobile} {
+    .quote__text {
+      font-size: 4.5rem;
+    }
+  }
+  @media (max-width: 375px) {
+    .quote__text {
+      /* font-size: calc([minimum size] + ([maximum size] - [minimum size]) * ((100vw - [minimum viewport width]) / ([maximum viewport width] - [minimum viewport width]))); */
+      font-size: 4.5rem;
+    }
   }
 `;
--- src/utils/breakpoints.js
+++ src/utils/breakpoints.js
@@ -1,9 +1,9 @@
 export const breakpoints = {
-  untilLaptop: `(max-width: 1079px)`,
-  forBigMobile: `(min-width: 399px)`,
-  forTablet: `(min-width: 599px)`,
-  forBigTablet: `(min-width: 699px)`,
-  forLaptop: `(min-width: 1079px) and (orientation: landscape)`,
-  forDesktop: `(min-width: 1439px)`,
   forBigDesktop: `(min-width: 1921px)`,
+  forBigMobile: `(max-width: 399px)`,
+  forBigTablet: `(max-width: 699px)`,
+  forDesktop: `(min-width: 1439px)`,
+  forLaptop: `(min-width: 1079px) and (orientation: landscape)`,
+  forTablet: `(min-width: 599px)`,
+  untilLaptop: `(max-width: 1079px)`,
 };
