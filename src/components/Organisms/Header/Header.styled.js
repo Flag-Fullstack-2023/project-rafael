d4cf5561d982ead23fc7b0dc47a81bb00eade975
kyuugoko <rafael.logrado@gmail.com>
2023-05-17 20:01:54 +0100

    hotfix

--- src/components/Organisms/Header/Header.styled.js
+++ src/components/Organisms/Header/Header.styled.js
@@ -1,13 +1,17 @@
 import styled from "styled-components";
+import { theme } from "../../../theme";
 
 export const HeaderEl = styled.header`
-  /* background-color: ${(props) => props.theme.darkGrey}; */
-
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  height: 4.8rem;
+  height: 8rem;
   padding: 1rem 3.2rem;
   position: fixed;
   width: 100%;
   z-index: 9999;
+  transition: all ease-in-out 0.4s;
+
+  &.header_bg {
+    background-color: ${theme.colors.darkGrey};
+  }
 `;
